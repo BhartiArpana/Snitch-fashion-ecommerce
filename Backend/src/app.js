78d2fb9a89f9bbe27828f9dacf49f80484b6bc58
Arpana bharti <arpanabharti09@gmail.com>
2026-07-02 15:33:51 +0530

    feat: implement user registration and Google OAuth authentication for backend and frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,17 +3,29 @@ import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
 import cors from 'cors'
+import passport from 'passport'
+import {Strategy as GoogleStrategy} from 'passport-google-oauth20'
+import {config} from './config/config.js'
 
 const app= express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cookieParser())
+app.use(passport.initialize())
 // app.use(cors({
 //     origin:'http://localhost:5173',
 //     methods:['GET','POST','PUT','DELETE','PATCH'],
 //     credentials:true
 // }))
+passport.use(new GoogleStrategy({
+    clientID : config.CLIENT_ID,
+    clientSecret : config.CLIENT_SECRET,
+    callbackURL : '/api/auth/google/callback'
+},(accessToken, refreshToken, profile, done)=>{
+   return done(null,profile)
+})
+)
 
 app.use('/api/auth',authRoutes)
 
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import {registerValidator,loginValidator} from '../validator/auth.validator.js'
 import {register,login} from '../controllers/auth.controller.js'
+import passport from 'passport'
 
 const router = express.Router()
 
 router.post('/register',registerValidator,register)
 router.post('/login',loginValidator,login)
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get('/google/callback',
+  passport.authenticate('google', { session: false })
+);
 
 export default router
