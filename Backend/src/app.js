0ae233fe970633798194fbafd1763ae19eb4deff
Arpana bharti <arpanabharti09@gmail.com>
2026-07-26 15:34:16 +0530

    feat: update CORS origin to production URL for API requests

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(passport.initialize())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://snitch-fashion-ecommerce-2.onrender.com',
     methods:['GET','POST','PUT','DELETE','PATCH'],
     credentials:true
 }))
